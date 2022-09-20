bc45981f4c64161d127b661ecf99dd433396474b
Michaela <malitherland@gmail.com>
2022-09-20 19:56:31 -0400

    exercise 5.8

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+
+
+const Blog = ({blog}) => {
+  
+const [toggle, setToggle] = useState(false);
+
+
+const handleView = (() => setToggle(!toggle))
+
+const showView = { display: toggle ? '' : 'none'}
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+
+  return(
+  <div style={blogStyle}>
+    {blog.title} by {blog.author} <button onClick={handleView}>view</button>
+    
+    <div style={showView}>
+      <p>url: {blog.url}</p>
+      <p>likes: {blog.likes}</p>
+      <button onClick={handleView}>hide</button>
+    </div>
+  </div>  )
+}
 
 export default Blog
