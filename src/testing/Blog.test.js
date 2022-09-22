64b35031d3034b7f0856e8d079714fe5588124f8
Michaela <malitherland@gmail.com>
2022-09-22 17:30:56 -0400

    exercise 5.14

--- src/testing/Blog.test.js
+++ src/testing/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
+import { getByText, render, screen } from '@testing-library/react'
+import userEvent from  '@testing-library/user-event'
 import Blog from '../components/Blog'
 //lets test first to see if the Blog component is rendered 
 
@@ -26,4 +27,20 @@ test('renders title and author name but not url or likes', () => {
     const { container } = render(<Blog blog={blog} />)
     const div = container.querySelector('.toggable')
     expect(div).toHaveStyle('display: none')
+})
+
+test('renders url and likes when \'view\' is clicked', async () => {
+    const blog = {
+        title: "I am a test",
+        author: "tester mcgee",
+        url: "am not real",
+        likes: 68
+    }
+    const user = userEvent.setup();
+    
+    const { container } = render(<Blog blog={blog} />)
+    const button = container.querySelector('button')
+    await user.click(button);
+    const div = container.querySelector('.toggable')
+    expect(div).not.toHaveStyle('display: none')
 })
